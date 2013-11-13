77981532576470f06c3aa4d8111d24e8f8f71c37
ibragimovrt <Rustam.Ibragimov@softline.ru>
2013-11-13 11:37:26 +0400

    таблица связи юзер-подразделение

--- src/Sir/OtBundle/Admin/UsersubdivisionAdmin.php
+++ src/Sir/OtBundle/Admin/UsersubdivisionAdmin.php
@@ -11,19 +11,20 @@ use Sonata\AdminBundle\Admin\Admin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Component\Security\Core\SecurityContextInterface;
 
 class UsersubdivisionAdmin extends Admin
 {
+
 	// Fields to be shown on create/edit forms
 	protected function configureFormFields(FormMapper $formMapper)
 	{
 		$formMapper
-			->add('usersubdivisions', 'entity', array('label' => 'Пользователь', 'class' => 'Sir\OtBundle\Entity\User', 'by_reference' => true))
+			->add('usersubdivisions', 'entity', array('label' => 'Пользователь', 'class' => 'Sir\OtBundle\Entity\User'))
 			->add('usersubdivisions', 'sonata_type_model', array('label' => 'Подразделение', 'class' => 'Sir\OtBundle\Entity\Subdivision',
 				'required' => true,
 				'multiple' => true,
-				'expanded' => true))
-		;
+				'expanded' => true));
 	}
 
 	// Fields to be shown on filter forms
--- src/Sir/OtBundle/SirOtBundle.php
+++ src/Sir/OtBundle/SirOtBundle.php
@@ -3,7 +3,9 @@
 namespace Sir\OtBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-
 class SirOtBundle extends Bundle
 {
+	public function __construct()
+	{
+	}
 }
