c8cf95ce37855a58274637d167aa94eec5ac7d79
myshevsv <myshev@gmail.com>
2013-11-26 23:25:44 +0400

    правка верстки, перевод слов, изменения в темплейтах

--- src/Sir/OtBundle/Controller/DefaultController.php
+++ src/Sir/OtBundle/Controller/DefaultController.php
@@ -8,7 +8,7 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-		$response = $this->forward('Sir\OtBundle\Controller\EquipmentController::indexAction');
+		$response = $this->forward('Sir\OtBundle\Controller\EnterpriseController::indexAction');
 		return $response;
 
 //		return $this->render('SirOtBundle:Default:index.html.twig');
