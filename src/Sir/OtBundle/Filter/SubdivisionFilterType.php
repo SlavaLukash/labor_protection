2f75b82f42e7ca68fc5eb1c36e52f59f4f08fc44
winder84 <winder84@mail.ru>
2013-12-02 13:37:41 +0400

    исправил баг в подразделениях

--- src/Sir/OtBundle/Filter/SubdivisionFilterType.php
+++ src/Sir/OtBundle/Filter/SubdivisionFilterType.php
@@ -18,7 +18,7 @@ class SubdivisionFilterType extends AbstractType
 	public function buildForm(FormBuilderInterface $builder, array $options)
 	{
 		$builder->add('enterprise', 'filter_text', array(
-			'apply_filter' => array($this, 'textFieldCallback')
+			'apply_filter' => array($this, 'enterpriseFieldCallback')
 		));
 		$builder->add('name', 'filter_text', array(
 			'apply_filter' => array($this, 'textFieldCallback')
@@ -38,13 +38,21 @@ class SubdivisionFilterType extends AbstractType
 		));
 	}
 
-	public function textFieldCallback(QueryInterface $filterQuery, $field, $values)
+	public function enterpriseFieldCallback(QueryInterface $filterQuery, $field, $values)
 	{
 		if (!empty($values['value'])) {
 			$qb = $filterQuery->getQueryBuilder();
 			$qb->innerJoin('e.enterprise', 'ee');
 			$qb->andWhere($filterQuery->getExpr()->like('ee.name', '\'%' .$values['value'] . '%\''));
 			$qb->orderBy('ee.id', 'ASC');
+		}
+	}
+
+	public function textFieldCallback(QueryInterface $filterQuery, $field, $values)
+	{
+		if (!empty($values['value'])) {
+			$qb = $filterQuery->getQueryBuilder();
+			$qb->andWhere($filterQuery->getExpr()->like($field, '\'%' .$values['value'] . '%\''));
 		}
 	}
 
