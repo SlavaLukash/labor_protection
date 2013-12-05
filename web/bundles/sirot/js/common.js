56b2eb1fc810031c85788f26b86035fa6d6ff7f9
Myshev <sergey.myshev@softline.ru>
2013-12-05 11:23:34 +0400

    стили и переводы

--- web/bundles/sirot/js/common.js
+++ web/bundles/sirot/js/common.js
@@ -10,6 +10,18 @@ $(document).ready(function () {
 			i++;
 		});
 	});
+
+	$.each($('table.record_properties'), function () {
+		var i = 0;
+		$.each($(this).children('tbody').children('tr'), function () {
+			if(i % 2 == 0) {
+				$(this).addClass('row_even');
+			} else {
+				$(this).addClass('row_odd');
+			}
+			i++;
+		});
+	});
 });
 
 function filterClear() {
