a364da8b8b7c27e8e7139358b18c75d68cf6b1d5
Myshev <sergey.myshev@softline.ru>
2013-12-12 15:53:42 +0400

    верстка кнопок(изменение размера бекграунда) + кнопка со ссылками на распечатку отчта в графике медосмотра

--- src/Sir/OtBundle/Resources/public/js/common.js
+++ src/Sir/OtBundle/Resources/public/js/common.js
@@ -32,6 +32,26 @@ $(document).ready(function () {
 		}
 		return false;
 	});
+
+	$('ul.button-list').hover(
+		function() {
+			var time = 0;
+
+			$.each($(this).find('li'), function () {
+				var elem = $(this);
+				setTimeout(function(){
+					if(!elem.hasClass('b-list-first-child')) elem.fadeIn(100);
+				}, time);
+				time += 100;
+			});
+			$(this).children('li').children('a').addClass('opened');
+		},
+		function () {
+			$(this).children('li').children('a').removeClass('opened');
+			$(this).children('li').hide();
+			$(this).children('li.b-list-first-child').show();
+		}
+	);
 });
 
 function filterClear() {
