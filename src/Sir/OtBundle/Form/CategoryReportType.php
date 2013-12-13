c3097de72f60076325d4fa936a372589ff5e0a64
Myshev <sergey.myshev@softline.ru>
2013-12-13 15:49:19 +0400

    категории отчетов и отчеты

---
+++ src/Sir/OtBundle/Form/CategoryReportType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Sir\OtBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class CategoryReportType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Sir\OtBundle\Entity\CategoryReport'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'sir_otbundle_categoryreport';
+    }
+}
---
+++ src/Sir/OtBundle/Form/ReportType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Sir\OtBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ReportType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('params')
+            ->add('categoryreport')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Sir\OtBundle\Entity\Report'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'sir_otbundle_report';
+    }
+}
